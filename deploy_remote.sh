1bbdcb7049f0160ea1dd83c0a96a448128425d0e
Afef0409 <68956845+Afef0409@users.noreply.github.com>
2021-05-19 14:59:15 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #deploy_remote.sh
 java -version
-if [[$? !=0 ]] then sudo amazon-linux-extras
-install -y java-openjdk11 fi
-#scp -i  /secret/kp-afef-2.pem  helloWorld.class ec2-user@ec2-13-56-228-246.us-west-1.compute.amazonaws.com:app
-ssh ec2-user@ec2-13-56-228-246.us-west-1.compute.amazonaws.com 
-cd app/ 
-java helloWorld
+if [[$? !=0 ]] then 
+sudo amazon-linux-extras install -y java-openjdk11 
+fi
+scp -i  /secret/kp-afef-2.pem  helloWorld.class ec2-user@ec2-13-56-228-246.us-west-1.compute.amazonaws.com:app
+ssh  -i  /secret/kp-afef-2.pem ec2-user@ec2-13-56-228-246.us-west-1.compute.amazonaws.com  'cd app/ && java helloWorld'
