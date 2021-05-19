0cde65d10cae2bcd8664a58eb8c9238678fcc0ba
Afef0409 <68956845+Afef0409@users.noreply.github.com>
2021-05-19 15:58:58 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 #deploy_remote.sh
 
-#Je mets la chaine de connexion de ma machine distance dans une variable
+echo Je mets la chaine de connexion de ma machine distance dans une variable
 MACHINE_DESTINATION="ec2-13-56-228-246.us-west-1.compute.amazonaws.com"
 PATH_SECRET_KEY="/secret/kp-afef-2.pem"
 
-#On installe Java sur la machine distante 
+sudo chmod 400 $PATH_SECRET_KEY
+
+echo On installe Java sur la machine distante 
 #ne pas oublier de mettre -o StrictHostKeyChecking=no pour ssh afin d'être non-interactif sur l'acception de la vérification de host
 #ne pas oublier de mettre -y pour yum ou amazon-linux-extras ou apt afin d'être non-interactif sur l'acception de l'installation
 ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo amazon-linux-extras install -y java-openjdk11'
 
-#On crée le répertoire /app sur la machine distante
+ echo On crée le répertoire /app sur la machine distante
 ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo mkdir -p /app && sudo chmod -R 777 /app && sudo chown ec2-user /app'
 
-#On copie le fichier .class dans le dossier homedir de l'utilisateur (car on n'arrive pas à l'envoyer directement sur /app)
+echo On copie le fichier .class dans le dossier homedir de l'utilisateur (car on n'arrive pas à l'envoyer directement sur /app)
 #possible de faire dans un dossier temporaire 
 scp -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY helloWorld.class $MACHINE_DESTINATION:/home/ec2-user/helloWorld.class
 
-#On copie le fichier .class dans le dossier /app localement 
+echo On copie le fichier .class dans le dossier /app localement 
 ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo cp /home/ec2-user/helloWorld.class /app'
 
-#On execute le fichier helloWorld.class
+echo On execute le fichier helloWorld.class
 ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo cd /app && sudo java helloWorld'
